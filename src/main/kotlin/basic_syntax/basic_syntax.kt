d25456c36aba696a920f62e7bddf54b12efdddbf
Mr235 <cnmr235@hotmail.com>
2017-09-29 14:50:11 +0800

    基本语法

--- src/main/kotlin/basic_syntax/basic_syntax.kt
+++ src/main/kotlin/basic_syntax/basic_syntax.kt
@@ -46,5 +46,119 @@ fun stringTemplates() {
     println("$s1\n$s2")
 }
 
+fun maxOf(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else{
+        return b
+    }
+}
 
+// if 做为方法表达式，不用return
+fun maxOf2(a: Int, b: Int) = if (a > b) a else b
 
+fun maxOf3(a: Int, b: Int) =
+        if (a > b){
+            // return a // 错误
+            a
+        } else {
+            b
+        }
+
+fun parseInt(str: String): Int? {
+    return str.toIntOrNull();
+}
+
+fun myParseInt(str: String): Int? {
+    val x = parseInt(str)
+    return x;
+}
+
+fun printProduct(arg1: String, arg2: String) {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+
+    if (x != null && y != null) {
+        println(x * y)
+    } else {
+        var str: String;
+        if (x == null) {
+            str = arg1
+        } else {
+            str = arg2
+        }
+        println("'$str' is not a number")
+    }
+}
+
+fun getStringLength(obj: Any): Int? = if(obj is String) obj.length else null
+
+fun useForLoop(arg: Int) {
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items)
+        println(item)
+}
+
+fun useForLoopIndex(arg: Int) {
+    val items = listOf("apple", "banana", "kiwi")
+    for (index in items.indices)
+        println("item at $index is ${items[index]}")
+}
+
+fun useWhileLoop(x: Any): Unit {
+    val items = listOf("apple", "banana", "kiwi")
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++;
+    }
+}
+
+fun describe(obj: Any): String =
+        when (obj) {
+            1 -> "One"
+            "Hello" -> "Greeting"
+            is Long -> "Long"
+            !is String -> "Not a string"
+            else -> "Unknown"
+        }
+
+fun useRanges(x: Any) {
+    val x = 10
+    val y = 9
+    if (x in 1 .. y + 1) {
+        println("fits in range ${1 .. y + 1}")
+    }
+}
+
+fun iteratingRange(x: Any) {
+    for (i in 1..5) {
+        print("$i ")
+    }
+    println()
+    for (i in 1..10 step 2) {
+        print("$i ")
+    }
+    println()
+    for (i in 9 downTo 0 step 2) {
+        print("$i ")
+    }
+}
+
+fun useCollections(x: Any) {
+    val items = listOf("apple", "avocado", "banana", "kiwi")
+    for (item in items) {
+        println(item)
+    }
+    println("------------------------")
+    when {
+        "banana" in items -> println("juicy")
+        "apple" in items -> println("apple is fine too")
+    }
+    println("------------------------")
+    items
+    .filter { it.startsWith("a") }
+    .sortedBy { it }
+    .map { it.toUpperCase() }
+    .forEach { println(it) }
+}
--- src/test/kotlin/basic_syntax/basic_syntax_test.kt
+++ src/test/kotlin/basic_syntax/basic_syntax_test.kt
@@ -1,5 +1,4 @@
 package basic_syntax
-import basic_syntax.localVariables
 import org.junit.Assert
 import org.junit.Test
 
@@ -9,16 +8,16 @@ class basic_syntax_test {
 //    }
 
     @Test fun sum() {
-        Assert.assertEquals(5, basic_syntax.sum(1, 4))
+        Assert.assertEquals(5, sum(1, 4))
     }
     @Test fun sum2() {
-        Assert.assertEquals(5, basic_syntax.sum2(1, 4))
+        Assert.assertEquals(5, sum2(1, 4))
     }
     @Test fun printSum() {
-        basic_syntax.printSum(1, 4)
+        printSum(1, 4)
     }
     @Test fun printSum2() {
-        basic_syntax.printSum2(1, 4)
+        printSum2(1, 4)
     }
     @Test fun localVariablesTest() {
         localVariables()
@@ -26,4 +25,56 @@ class basic_syntax_test {
     @Test fun stringTemplatesTest() {
         stringTemplates()
     }
+
+    @Test fun maxOf() {
+        Assert.assertEquals(4, maxOf(1, 4))
+    }
+
+    @Test fun maxOf2() {
+        Assert.assertEquals(4, maxOf2(1, 4))
+    }
+
+    @Test fun maxOf3() {
+        Assert.assertEquals(4, maxOf3(1, 4))
+    }
+
+    @Test fun myParseInt() {
+        Assert.assertEquals(3, myParseInt("4"))
+    }
+
+    @Test fun printProduct() {
+        printProduct("4", "a5")
+    }
+
+    @Test fun getStringLength() {
+        Assert.assertEquals(3, getStringLength("abc"))
+    }
+
+    @Test fun useForLoop() {
+        useForLoop(1)
+    }
+
+    @Test fun useForLoopIndex() {
+        useForLoopIndex(1)
+    }
+
+    @Test fun useWhileLoop() {
+        useWhileLoop(1)
+    }
+
+    @Test fun describe() {
+        val describe = describe("Hello")
+        println(describe)
+    }
+    @Test fun useRanges() {
+        useRanges(1)
+    }
+    @Test fun iteratingRange() {
+        iteratingRange(1)
+    }
+
+    @Test fun useCollections() {
+        useCollections(1)
+    }
+
 }
